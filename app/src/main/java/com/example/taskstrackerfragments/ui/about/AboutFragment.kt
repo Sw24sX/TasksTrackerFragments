6b6c8c7ff75f0eefcba5350a41f0929bda993629
Sw24sX <aleksandr.korolyov.99@mail.ru>
2021-04-03 01:09:20 +0500

    rename in nav menu

--- app/src/main/java/com/example/taskstrackerfragments/ui/about/AboutFragment.kt
+++ app/src/main/java/com/example/taskstrackerfragments/ui/about/AboutFragment.kt
@@ -6,8 +6,6 @@ import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
 import androidx.fragment.app.Fragment
-import androidx.lifecycle.Observer
-import androidx.lifecycle.ViewModelProvider
 import com.example.taskstrackerfragments.R
 
 class AboutFragment : Fragment() {
@@ -21,7 +19,7 @@ class AboutFragment : Fragment() {
     ): View? {
         //slideshowViewModel =
         //        ViewModelProvider(this).get(SlideshowViewModel::class.java)
-        val root = inflater.inflate(R.layout.fragment_slideshow, container, false)
+        val root = inflater.inflate(R.layout.fragment_about, container, false)
         val textView: TextView = root.findViewById(R.id.text_slideshow)
         //slideshowViewModel.text.observe(viewLifecycleOwner, Observer {
         //    textView.text = it
